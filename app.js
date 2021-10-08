6ddef69ddf74a71830a3e8110849d1faa0f9c935
alisherfw <alisherfw@gmail.com>
2021-10-08 23:52:46 +0900

    nodemon

--- app.js
+++ app.js
@@ -1,15 +1,7 @@
-const http = require('http');
+const _ = require('lodash');
 
-const server = http.createServer((req, res) => {
-    if(req.url === '/') {
-        res.end('Home Page');
-    }
-    if(req.url === '/about') {
-        res.end('About Page');
-    }
-    res.end(`<h1> OOps! </h1>
-    <p> 404 error. Page not found! </p>
-    <a href="/"> Back to the Home </a>`);
-});
+const item = [1, [2, [3, [4]]]];
+const newItems = _.flattenDeep(item);
 
-server.listen(3000);
+console.log(newItems);
+console.log("hello jjj");
