0f33e6a246f5769302e0005a338bb00abc327e9e
alisherfw <alisherfw@gmail.com>
2021-10-10 23:55:27 +0900

    blocking code example

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const http = require('http');
- 
+
 const server = http.createServer((req, res) => {
-    console.log('request event');
-    res.end('Hello World');
-})
+    if(req.url === '/') {
+        res.end("Home Page");
+    }
+    else if(req.url === '/about') {
+        for(let i = 0; i < 1000; i++)
+            for(let j = 0; j < 1000; j++) 
+                console.log(`${j}, ${i}`);
+        res.end("About Page");
+    } 
+});
 
 server.listen(5000, () => {
-    console.log('server is running on port 5000');
+    console.log('server is running on port: 5000');
 })
